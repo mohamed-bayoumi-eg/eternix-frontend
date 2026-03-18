1796909a513b867a3e6e481daf34acc6002cb36d
mohamed-bayoumi-eg <mohamed.bayoumi.eg@gmail.com>
2026-03-18 13:54:45 +0200

    handle select details

--- src/app/shared/components/dynamic-components/dynamic-details-table-component/dynamic-details-table-component.ts
+++ src/app/shared/components/dynamic-components/dynamic-details-table-component/dynamic-details-table-component.ts
@@ -49,47 +49,59 @@ export class DynamicDetailsTableComponent implements OnInit, OnChanges {
   }
   private checkValidity() {
     setTimeout(() => {
-      const allRowsValid = this.rowForms.length > 0 ? this.rowForms.every((f) => f.valid) : true;
+      if (this.rowForms.length === 0 && this.config.required) {
+        this.statusChange.emit(false);
+        return;
+      }
 
+      const allRowsValid = this.rowForms.every((f) => f.valid);
       const hasData = this.config.data().length > 0;
       const finalStatus = this.config.required ? hasData && allRowsValid : allRowsValid;
 
       this.statusChange.emit(finalStatus);
       this.cdr.detectChanges();
-    }, 0);
+    });
   }
-
   getRowForm(index: number, row: any): FormGroup {
-    if (!this.rowForms[index]) {
-      const group = new FormGroup({});
-      this.config.columns.forEach((col) => {
-        const control = new FormControl(row[col.fieldName]);
-console.log(col)
-        group.addControl(col.fieldName, control);
-
-        control.valueChanges.subscribe((value) => {
-          this.updateValue(index, col.fieldName, value);
-        });
-         if (row[col.fieldName] !== undefined && row[col.fieldName] !== null) {
-          setTimeout(() => {
-            control.markAsTouched();
-            control.updateValueAndValidity();
-          });
-        }
-      });
-
-      group.statusChanges.subscribe(() => this.checkValidity());
-      this.rowForms[index] = group;
-    } else {
+    if (this.rowForms[index]) {
       const group = this.rowForms[index];
       this.config.columns.forEach((col) => {
-        if (group.get(col.fieldName)?.value !== row[col.fieldName]) {
-          group.get(col.fieldName)?.setValue(row[col.fieldName], { emitEvent: false });
+        const control = group.get(col.fieldName);
+        if (control && control.value !== row[col.fieldName]) {
+          control.setValue(row[col.fieldName], { emitEvent: false });
         }
       });
-      
+      return group;
     }
-    return this.rowForms[index];
+
+    const group = new FormGroup({});
+    this.config.columns.forEach((col) => {
+      const control = new FormControl(row[col.fieldName]);
+      group.addControl(col.fieldName, control);
+
+      control.valueChanges.subscribe((value) => {
+        this.updateValue(index, col.fieldName, value);
+      });
+
+      if (
+        row[col.fieldName] !== undefined &&
+        row[col.fieldName] !== null &&
+        row[col.fieldName] !== ''
+      ) {
+        setTimeout(() => {
+          control.markAsTouched();
+          control.markAsDirty(); 
+          control.updateValueAndValidity({ emitEvent: true });
+        });
+      }
+    });
+
+    group.statusChanges.subscribe(() => this.checkValidity());
+    this.rowForms[index] = group;
+
+    setTimeout(() => this.checkValidity());
+
+    return group;
   }
 
   updateValue(index: number, fieldName: string, value: any) {
